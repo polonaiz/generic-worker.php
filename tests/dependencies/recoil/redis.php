9f46c42c8a779f3e217733837a16910271a23ac4
polonaiz <polonaiz@gmail.com>
2020-07-20 20:16:34 +0900

    add some test code

--- tests/dependencies/recoil/redis.php
+++ tests/dependencies/recoil/redis.php
@@ -27,22 +27,36 @@ ReactKernel::start(function () use ($adder, $subtractor)
 	{
 		$factory = new \Clue\React\Redis\Factory(yield Recoil::eventLoop());
 		$client = yield $factory->createClient('localhost');
-		yield [
+
+		// parallel execution
+		yield Recoil::all([
 			function () use ($client, $adder)
 				{
+					// client side - submit task
 					yield $client->set('task1', $task = \json_encode(['closure' => serializeClosure($adder), 'param' => [1, 2]]));
 
+					// worker side - execute task
 					$task = \json_decode(yield $client->get('task1'), true);
 					$result = \call_user_func_array(\unserialize($task['closure']), $task['param']);
-					echo $result . PHP_EOL;
+					yield $client->set('task1_result', \json_encode($result));
+
+					// client side - get task result
+					$result = yield $client->get('task1_result');
+					echo 'adder result: ' . $result . PHP_EOL;
 				},
 			function () use ($client, $subtractor)
 				{
+					// client side - submit task
 					yield $client->set('task2', $task = \json_encode(['closure' => serializeClosure($subtractor), 'param' => [3, 1]]));
 
+					// worker side - execute task
 					$task = \json_decode(yield $client->get('task2'), true);
 					$result = \call_user_func_array(\unserialize($task['closure']), $task['param']);
-					echo $result . PHP_EOL;
+					yield $client->set('task2_result', \json_encode($result));
+
+					// client side - get task result
+					$result = yield $client->get('task2_result');
+					echo 'subtractor result: ' . $result . PHP_EOL;
 				}
-		];
+		]);
 	});
